59e845adf9a35496888faabea4c68e61148dcde0
maxim760 <maks.tomanov@mail.ru>
2021-03-02 01:28:16 +0300

    commit

--- src/function/lang/index.test.ts
+++ src/function/lang/index.test.ts
@@ -1,5 +1,23 @@
-import { castArray, clone, cloneDeep } from ".";
-
+import {
+  castArray,
+  clone,
+  cloneDeep,
+  isArrayLike,
+  isArrayLikeObject,
+  isBuffer,
+  gt,
+  eq,
+  gte,
+  isElement,
+  isEmpty,
+  isEqual,
+  isArguments,
+  isArray,
+  isArrayBuffer,
+  isBoolean,
+  isDate,
+  conformsTo,
+} from ".";
 describe("lang", () => {
   describe("castArray", () => {
     test("number", () => {
@@ -43,29 +61,210 @@ describe("lang", () => {
       expect(objects[0] === shallow[0]).toBeFalse();
     });
   });
-  describe('clone deep with', () => {
-    
-  })
-  
+  describe("conformsTo", () => {
+    var object = { a: 1, b: 2 };
+
+    it("falsey func", () => {
+      expect(
+        conformsTo(object, {
+          b: function (n: any) {
+            return n > 2;
+          },
+        })
+      ).toBeFalse();
+    });
+    it("true func", () => {
+      expect(
+        conformsTo(object, {
+          b: function (n: any) {
+            return n > 1;
+          },
+        })
+      ).toBeTrue();
+    });
+  });
+  describe("eq", () => {
+    var object = { a: 1 };
+    var other = { a: 1 };
+    it("object one ref", () => {
+      expect(eq(object, object)).toBeTrue();
+    });
+    it("different object", () => {
+      expect(eq(object, other)).toBeFalse();
+    });
+    it("string", () => {
+      expect(eq("a", "a")).toBeTrue();
+    });
+    it("string and object string", () => {
+      expect(eq("a", Object("a"))).toBeFalse();
+    });
+    it("nan", () => {
+      expect(eq(NaN, NaN)).toBeTrue();
+    });
+  });
+  describe("gt", () => {
+    it("gt test", () => {
+      expect(gt(3, 1)).toBeTrue();
+
+      expect(gt(3, 3)).toBeFalse();
+      expect(gt(1, 3)).toBeFalse();
+    });
+  });
+  describe("gte", () => {
+    it("gte test", () => {
+      expect(gte(3, 1)).toBeTrue();
+      // => true
+
+      expect(gte(3, 3)).toBeTrue();
+      // => true
+
+      expect(gte(1, 3)).toBeFalse();
+      // => false
+    });
+  });
+  describe("isArguments", () => {
+    it("array", () => {
+      expect(
+        isArguments(
+          (function () {
+            return arguments;
+          })()
+        )
+      ).toBeTrue();
+    });
+    it("arguments", () => {
+      expect(isArguments([1, 2, 3])).toBeFalse();
+    });
+  });
+  describe("isArray", () => {
+    it("array", () => {
+      expect(isArray([1, 2, 3])).toBeTrue();
+    });
+    it("children", () => {
+      expect(isArray(document.body.children)).toBeFalse();
+    });
+    it("string", () => {
+      expect(isArray("abc")).toBeFalse();
+    });
+    it("null", () => {
+      expect(isArray("abc")).toBeFalse;
+    });
+  });
+  describe("isArrayBuffer", () => {
+    it("array", () => {
+      expect(isArrayBuffer([1, 2, 3])).toBeFalse();
+      expect(isArrayBuffer(new Array(2))).toBeFalse();
+    });
+    it("arrayBuffer", () => {
+      expect(isArrayBuffer(new ArrayBuffer(2))).toBeTrue();
+    });
+  });
+  describe("isArrayLike", () => {
+    it("array", () => {
+      expect(isArrayLike([1, 2, 3])).toBeTrue();
+    });
+    it("set", () => {
+      expect(isArrayLike(new Set([1,2,3,1,2,3]))).toBeTrue();
+    });
+    it("children", () => {
+      expect(isArrayLike(document.body.children)).toBeTrue();
+    });
+    it("string", () => {
+      expect(isArrayLike("abc")).toBeTrue();
+    });
+    it("null", () => {
+      expect(isArrayLike("abc")).toBeFalse;
+    });
+  });
+  describe("isArrayLikeObject", () => {
+    it("array", () => {
+      expect(isArrayLikeObject([1, 2, 3])).toBeTrue();
+    });
+    it("set", () => {
+      expect(isArrayLikeObject(new Set([1,2,3,1,2,3]))).toBeTrue();
+    });
+    it("string", () => {
+      expect(isArrayLikeObject("abc")).toBeFalse();
+    });
+    it("null", () => {
+      expect(isArrayLikeObject("abc")).toBeFalse;
+    });
+  });
+  describe("isBoolean", () => {
+    it("boolean", () => {
+      expect(isBoolean(true)).toBeTrue();
+      expect(isBoolean(false)).toBeTrue();
+    });
+    it("not boolean", () => {
+      expect(isBoolean("true")).toBeFalse();
+      expect(isBoolean(123)).toBeFalse();
+      expect(isBoolean([])).toBeFalse();
+      expect(isBoolean({})).toBeFalse();
+      expect(isBoolean(NaN)).toBeFalse();
+      expect(isBoolean(null)).toBeFalse();
+    });
+  });
+  describe("isBuffer", () => {
+    it("buffer", () => {
+      expect(isBuffer(new Buffer(2))).toBeTrue();
+    });
+    it("not buffer", () => {
+      expect(isBuffer(new Uint16Array())).toBeFalse();
+      expect(isBuffer(new Uint32Array())).toBeFalse();
+      expect(isBuffer(new Uint8ClampedArray())).toBeFalse();
+      expect(isBuffer(new Array(3))).toBeFalse();
+      expect(isBuffer(new ArrayBuffer(2))).toBeFalse();
+    });
+    describe("isDate", () => {
+      it("date", () => {
+        expect(isDate(new Date())).toBeTrue();
+      });
+      it("string", () => {
+        expect(isDate("Mon April 23 2012")).toBeFalse();
+      });
+    });
+    describe("isElement", () => {
+      it("success", () => {
+        expect(isElement(document.body)).toBeTrue();
+      });
+      it("fail", () => {
+        expect(isElement("<body>")).toBeFalse();
+      });
+    });
+    describe("isEmpty", () => {
+      it("null", () => {
+        expect(isEmpty(null)).toBeTrue();
+      });
+      it("number", () => {
+        expect(isEmpty(122)).toBeTrue();
+        expect(isEmpty(0)).toBeTrue();
+        expect(isEmpty(-99)).toBeTrue();
+      });
+      it("boolean", () => {
+        expect(isEmpty(false)).toBeTrue();
+        expect(isEmpty(true)).toBeTrue();
+      });
+      it("array", () => {
+        expect(isEmpty([{}])).toBeFalse();
+        expect(isEmpty([1, 2, 3])).toBeFalse();
+      });
+      it("empty array", () => {
+        expect(isEmpty([])).toBeTrue();
+      });
+      it("object", () => {
+        expect(isEmpty({ a: 1 })).toBeFalse();
+      });
+      it("empty object", () => {
+        expect(isEmpty({})).toBeTrue();
+      });
+    });
+    describe("isEqual", () => {
+      var object = { a: 1 };
+      var other = { a: 1 };
+
+      it("success equal compare", () => {
+        expect(isEqual(object, other)).toBeTrue();
+      });
+    });
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/function/lang/index.ts
+++ src/function/lang/index.ts
@@ -1,3 +1,5 @@
+import { Buffer } from "buffer";
+
 export const castArray = (...args: any) => {
   console.log(args);
   if (args.length === 0) {
@@ -29,40 +31,132 @@ export const cloneDeepWith = (value: any, customizer: any) => {
   customizer = typeof customizer === "function" ? customizer : undefined;
   const deepCopy = (item: any) =>
     typeof item === "object" ? JSON.parse(JSON.stringify(item)) : item;
-  const res = deepCopy(value)
+  const res = deepCopy(value);
   if (!customizer) {
-    return res
+    return res;
   }
   if (typeof value !== "object") {
-    return value
+    return value;
   }
   if (Array.isArray(value)) {
-    return value.map((item ,i , array) => customizer(item, i, array))
+    return value.map((item, i, array) => customizer(item, i, array));
   }
-  return Object.values(value).map((item, i) => customizer(item, i, value))
+  return Object.values(value).map((item, i) => customizer(item, i, value));
   // не полностью понял как именно работает customizer
-}
+};
 export const cloneWith = (value: any, customizer: any) => {
   customizer = typeof customizer === "function" ? customizer : undefined;
   const shallowCopy = (item: any) =>
     Array.isArray(item)
       ? [...item]
       : typeof item === "object"
-        ? { ...item }
-        : item;
-  const res = shallowCopy(value)
+      ? { ...item }
+      : item;
+  const res = shallowCopy(value);
   if (!customizer) {
-    return res
+    return res;
   }
   if (typeof value !== "object") {
-    return value
+    return value;
   }
   if (Array.isArray(value)) {
-    return value.map((item ,i , array) => customizer(item, i, array))
+    return value.map((item, i, array) => customizer(item, i, array));
   }
-  return Object.values(value).map((item, i) => customizer(item, i, value))
+  return Object.values(value).map((item, i) => customizer(item, i, value));
   // не полностью понял как именно работает customizer
-}
+};
+export type IObject = { [key: string]: any };
+
+export const conformsTo = (object: IObject, sourse: IObject) => {
+  const check = (key: string) => {
+    return typeof sourse[key] === "function"
+      ? sourse[key](object[key])
+      : sourse[key] === object[key];
+  };
+  return !Object.keys(sourse).some((key) => {
+    return !check(key);
+  });
+};
+
+export const eq = (value: any, other: any) => {
+  return value === other || (value !== value && other !== other);
+};
+
+export const gt = (value: number, other: number) => value > other;
+export const gte = (value: number, other: number) => value >= other;
+
+export const isArguments = (value: any) => {
+  return (
+    value.toString() ===
+    (function (a, b, c) {
+      return arguments;
+    })().toString()
+  );
+};
+
+export const isArray = (value: any) => {
+  return Array.isArray(value);
+};
+
+export const isArrayBuffer = (value: any) => {
+  return value instanceof ArrayBuffer;
+};
+
+export const isArrayLike = (value: any) => {
+  return (value?.length ?? value?.size ?? undefined) !== undefined;
+};
+
+export const isArrayLikeObject = (value: any) => {
+  return (
+    typeof value === "object" && ((value?.length ?? value?.size ?? undefined) !== undefined)
+  );
+};
 
 
+export const isBoolean = (value: any) => typeof value === "boolean";
 
+export const isBuffer = (value: any) => value instanceof Buffer;
+
+export const isDate = (value: any) => value instanceof Date;
+
+export const isElement = (value: any) =>
+  value?.children instanceof HTMLCollection;
+
+export const isEmpty = (value: any) =>
+  typeof value === "number" || typeof value === "boolean"
+    ? true
+    : typeof value === "object" && value !== null
+    ? Array.isArray(value)
+      ? value.length === 0
+      : Object.keys(value).length === 0
+    : !value;
+
+export const isEqual = (value: any, other: any): boolean => {
+  let isEq = true;
+  if (value === other || (value !== value && other !== other)) {
+    return true;
+  }
+  if (
+    typeof value !== typeof other ||
+    typeof value !== "object" ||
+    Array.isArray(value) !== Array.isArray(other)
+  ) {
+    return false;
+  }
+  if (Array.isArray(value)) {
+    if (other.length !== value.length) {
+      return false
+    }
+    value.forEach((item, i) => {
+      isEq = isEq && isEqual(item, other[i]);
+    });
+  } else {
+    if (Object.keys(value).length !== Object.keys(other).length) {
+      return false
+    }
+    Object.keys(value).forEach((key: string) => {
+      isEq = isEq && isEqual(value[key], other[key])
+    })
+  }
+  return isEq;
+};
